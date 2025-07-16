7c0428f85620a5fdbcf6b8f4e986a008d89c0845
Kathan Parekh <parekhkathan5@gmail.com>
2025-07-16 15:35:04 -0400

    the_change_before_commit...

--- js/miner.js
+++ js/miner.js
@@ -1,12 +1,12 @@
 async function mineBlock() {
   const address = document.getElementById('miner').value;
-  const res = await fetch(`https://localhost:4433/mine/${address}`, { method: 'POST' });
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/wallet/create/mine/${address}`, { method: 'POST' });
   const data = await res.json();
   document.getElementById('mineOutput').textContent = data.message || data.error;
 }
 
 async function getChain() {
-  const res = await fetch(`https://localhost:4433/chain`);
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/wallet/create/chain`);
   const data = await res.json();
   document.getElementById('chainOutput').textContent = JSON.stringify(data, null, 2);
 }
--- js/user.js
+++ js/user.js
@@ -1,5 +1,5 @@
 async function createWallet() {
-  const res = await fetch(`https://localhost:4433/wallet/create`, { method: 'POST' });
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/wallet/create`, { method: 'POST' });
   const data = await res.json();
   document.getElementById('address').value = data.address;
   document.getElementById('privateKey').value = data.privateKey;
@@ -7,7 +7,7 @@ async function createWallet() {
 
 async function getBalance() {
   const address = document.getElementById('balanceAddress').value;
-  const res = await fetch(`https://localhost:4433/wallet/balance/${address}`);
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/wallet/balance/${address}`);
   const data = await res.json();
   document.getElementById('balanceOutput').textContent = `Balance: ${data.balance}`;
 }
@@ -19,7 +19,7 @@ async function sendTransaction() {
     amount: Number(document.getElementById('amount').value),
     privateKey: document.getElementById('key').value
   };
-  const res = await fetch(`https://localhost:4433/transaction/send`, {
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/transaction/send`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
