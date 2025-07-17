035b8374317b2a7b4a72742c15dccf4417f73043
Kathan Parekh <parekhkathan5@gmail.com>
2025-07-17 15:16:31 -0400

    commit

--- js/miner.js
+++ js/miner.js
@@ -1,12 +1,12 @@
 async function mineBlock() {
   const address = document.getElementById('miner').value;
-  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/wallet/mine/${address}`, { method: 'POST' });
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/mine/${address}`, { method: 'POST' });
   const data = await res.json();
   document.getElementById('mineOutput').textContent = data.message || data.error;
 }
 
 async function getChain() {
-  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/wallet/chain`);
+  const res = await fetch(`https://https-github-com-themrgeek-pana-2.onrender.com/chain`);
   const data = await res.json();
   document.getElementById('chainOutput').textContent = JSON.stringify(data, null, 2);
 }
